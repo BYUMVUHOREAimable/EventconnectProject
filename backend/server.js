16921564b5eed8cf8f0aacb7e2afca71405c3227
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2024-06-02 21:04:09 +0200

    allowed all origins on the backend

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
