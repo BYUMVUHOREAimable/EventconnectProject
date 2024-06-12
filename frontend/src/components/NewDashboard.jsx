1448eb954f3273a655bc74de967694d8bb161afc
BYUMVUHORE Aimable <aimablebyumvuhore@gmail.com>
2024-06-12 16:49:46 +0200

    updating dashboard page

--- frontend/src/components/NewDashboard.jsx
+++ frontend/src/components/NewDashboard.jsx
@@ -4,13 +4,11 @@ import MainContentDashboard from './SubComponents/MainContentDashboard';
 import SidebarDashboard from './SubComponents/SiderbarDashboard';
 import NavbarDash from './NavbarDash';
 import Footer from "./Footer";
-import Navbar from './Navbar';
 
 const NewDashboard = () => {
   return (
     <div>
       <div className="flex h-screen overflow-hidden">
-        <Navbar/>
       <div className='mt-14 z-20'>
         <NavbarDash/>
       </div>
